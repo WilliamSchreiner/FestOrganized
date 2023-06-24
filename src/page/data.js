e286390525d57783448902b57c03bca45750a77e
MatheusFoliatti <matheusfoliatti@gmail.com>
2023-06-23 21:38:55 -0300

    valores no nome

--- src/page/data.js
+++ src/page/data.js
@@ -2,13 +2,13 @@
 
 
 export const data = [
-    {"key":1, "value":"Quentão - tamanho P",  "valor": 5},
-    {"key":2, "value": "Quentão - tamanho M",  "valor":5},
-    {"key":3, "value":"Quentão - tamanho G",  "valor": 5},
-    {"key": 4, "value":"Pipoca - tamanho P",  "valor": 5},
-    {"key":5, "value":"Pipoca - tamanho M",  "valor": 5},
-    {"key":6, "value":"Pipoca - tamanho G",  "valor": 5},
-    {"key":7, "value":"Bolo", "valor": 5},
-    {"key":8, "value":"Pé de moleque",  "valor": 5},
-    {"key":9, "value":"Cachorro quente",  "valor": 5}
+    {"key":1, "value":"Quentão - tamanho P - R$5,00",  "valor": 5},
+    {"key":2, "value": "Quentão - tamanho M - R$5,00",  "valor":5},
+    {"key":3, "value":"Quentão - tamanho G - R$5,00",  "valor": 5},
+    {"key": 4, "value":"Pipoca - tamanho P - R$5,00",  "valor": 5},
+    {"key":5, "value":"Pipoca - tamanho M - R$5,00",  "valor": 5},
+    {"key":6, "value":"Pipoca - tamanho G - R$5,00",  "valor": 5},
+    {"key":7, "value":"Bolo - R$5,00", "valor": 5},
+    {"key":8, "value":"Pé de moleque - R$5,00",  "valor": 5},
+    {"key":9, "value":"Cachorro quente - R$5,00",  "valor": 5}
 ]
